cd849eea8ed515891bd67bba0ff5971b3d81860f
Frank Zarrinkelk <fzarrinkelk@hotmail.com>
2025-03-28 14:51:52 -0700

    Init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "edu.fullerton.fz.cs411.listwithrecyclerview01"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "edu.fullerton.fz.cs411.listwithrecyclerview01"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -37,12 +37,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.lifecycle:lifecycle-extensions:2.0.0")
-    implementation("androidx.recyclerview:recyclerview:1.2.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
